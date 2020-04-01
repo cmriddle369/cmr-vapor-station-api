eee5d88b7c34688110a4c5047bb2c73b4cf546c5
Dorychop Riddle <cmriddle369@gmail.com>
2020-04-01 16:55:52 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,49 +1,58 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_heroku import Heroku 
 from flask_marshmallow import Marshmallow
-import os
 
 app = Flask(__name__)
 
-# basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://xfmozpoqnagdet:7f2a1ae5ad04e23f3855f26f365346f57c40b13b004fcbb3ad7fbd58739977a3@ec2-52-71-85-210.compute-1.amazonaws.com:5432/d73c8dhaa1ln0j"
+
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+heroku = Heroku(app)
 CORS(app)
 
-class Form(db.Model):
-    id = db.Column(db.Integer, primary_key =True)
-    name = db.Column(db.String(100), unique=False)
-    email = db.Column(db.String, unique=False)
-    message= db.Column(db.String, unique=False)
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key =True, unique=True)
+    name = db.Column(db.String(100), nullable=False, unique=False)
+    email = db.Column(db.String, nullable=False, unique=False)
+    message= db.Column(db.String, nullable=False, unique=False)
 
     def __init__(self, name, email, message):
         self.name = name
         self.email = email
         self.message = message
 
-# class FormSchema(ma.Schema):
-#     class Meta:
-#         fields = ('id', 'name', 'email', 'message')
+class PostSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "email", "message")
 
-# contact_schema = ContactSchema()
-# contacts_Schema = ContactSchema(many=True)
+post_schema = PostSchema()
+posts_schema = PostSchema(many=True)
 
-# @app.route('/form', methods=['POST'])
-# def add_form():
-#     name = request.json['name']
-#     email = request.json['email']
-#     message = request.json['message']
+@app.route("/contact/post", methods=["POST"])
+def create_message():
+    if request.content_type != "application/json":
+        return jsonify("Error: Data must be sent as JSON.")
 
-#     new_form = Form(name, email, message)
+    post_data = request.get_json()
+    name = post_data.get("name")
+    email = post_data.get("email")
+    message = post_data.get("message")
 
-#     db.session.add(new_form)
-#     db.session.commit()
+    record = Post(name, email, message)
+    db.session.add(record)
+    db.session.commit()
 
-#     form = Form.query.get(new_form.id)
+    return jsonify("Message Created")
 
-#     return form_schema.jsonify(form)
+@app.route("/contact/get", methods=["GET"])
+def get_all_messages():
+    all_messages = db.session.query(Post).all()
+    return jsonify(post_schema.dump(all_messages))
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
